5457dff08662fb996c4caa9ab575134820e98a39
Hong Ngoc Doan Thi <mailtocuong@gmail.com>
2020-08-21 13:55:36 +0300

    add dummy data

---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+
+const Product = (product) => {
+	const dispatch = useDispatch();
+    const [inventory, setInventory] = useState(product.inventory);
+    const [errorMessage,setErrorMessage] = useState()
+
+
+};
+
+
